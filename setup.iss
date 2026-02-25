55d27029e0e04f8ae61951f40f7e5f037d0731d2
Prod Aki <prodaki@stemsorganizer.pro>
2026-02-25 00:45:57 -0300

    v1.7.7: Regenerated logo2.ico with proper multi-resolution ICO format (16-256px)

--- setup.iss
+++ setup.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=Stems Organizer PRO
-AppVersion=1.7.5
+AppVersion=1.7.7
 AppPublisher=Prod. Aki
 AppPublisherURL=https://github.com/Davidwhs01/Stems-Organizer-PRO
 DefaultDirName={autopf}\Stems Organizer PRO
--- stems_organizer_pro/config.py
+++ stems_organizer_pro/config.py
@@ -5,7 +5,7 @@ Constantes, caminhos, credenciais, paleta de cores.
 import os
 
 # --- VERSÃO E REPOSITÓRIO ---
-CURRENT_VERSION = "1.7.6"
+CURRENT_VERSION = "1.7.7"
 GITHUB_REPO = "Davidwhs01/Stems-Organizer-PRO"
 
 # --- CAMINHOS ---
